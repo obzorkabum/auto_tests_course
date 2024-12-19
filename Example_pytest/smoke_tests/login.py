e13db6f8d8774c2bd6f9e82c4c4157ec22c4cc60
Kirill <obzorkabum@gmail.com>
2024-12-19 11:12:57 +0300

    add solve lesson 3, step 3, substep 6

---
+++ Example_pytest/smoke_tests/login.py
@@ -0,0 +1,8 @@
+# номер 3
+def test_guest_can_login(browser, language):
+    pass
+
+# номер 4
+class TestLogin(object):
+    def test_guest_should_see_login_link(self, browser, language):
+        pass
---
+++ Example_pytest/test_regression.py
@@ -0,0 +1,18 @@
+class TestLessonCreate():
+    # номер 5
+    def test_create_lesson(self, browser):
+        pass
+
+    # номер 6
+    def user_with_lesson_can_create_lesson_from_navbar_test(self, browser):
+        pass
+
+
+class CourseCreate():
+    # номер 7
+    def test_create_course(self, browser):
+        pass
+
+# номер 8
+def test_guest_can_open_new_course(browser):
+    pass
