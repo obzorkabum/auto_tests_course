f0104710d9328f4386242718805a4b5c66d0c180
Kirill <obzorkabum@gmail.com>
2024-12-19 12:49:16 +0300

    add comments with pytest inicialization

--- step3/test_fixture8.py
+++ step3/test_fixture8.py
@@ -25,3 +25,6 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+# pytest - s - v - m "not smoke" test_fixture8.py
+# pytest -s -v -m "smoke or regression" test_fixture8.py
--- step3/test_fixture81.py
+++ step3/test_fixture81.py
@@ -26,3 +26,5 @@ class TestMainPage1:
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+# pytest -s -v -m "smoke and win10" test_fixture81.py
